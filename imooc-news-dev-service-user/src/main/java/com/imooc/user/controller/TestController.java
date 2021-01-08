0ddb4d38e4333c00e25d5c8434235ea450ffb801
SoSo <LuckilyChensy@163.com>
2021-01-08 22:29:04 +0800

    imooc-news-dev 日志引用

--- imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/TestController.java
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/TestController.java
@@ -1,14 +1,25 @@
 package com.imooc.user.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.imooc.api.controller.user.TestControllerApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class TestController implements TestControllerApi {
 
-    @GetMapping("/test")
+    final static Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @Override
     public Object test(){
+
+        logger.debug("debug: test spring framework ");
+        logger.info("info: test spring framework ");
+        logger.warn("warn: test spring framework ");
+        logger.error("error: test spring framework ");
+
         return "<h3> test spring framework </h3>";
+
     }
 
 }
