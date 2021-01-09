5e99daad345278b1b70021cdef6981ecb5d576a8
SoSo <LuckilyChensy@163.com>
2021-01-09 20:51:00 +0800

    imooc-news-dev common Exception

---
+++ imooc-news-dev-common/src/main/java/com/imooc/exception/GraceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.imooc.exception;
+
+import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.grace.result.ResponseStatusEnum;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@ControllerAdvice
+public class GraceExceptionHandler {
+
+    @ExceptionHandler(MyCustomException.class)
+    @ResponseBody
+    public GraceJSONResult returnMyException(MyCustomException e) {
+        e.printStackTrace();
+        return GraceJSONResult.exception(e.getResponseStatusEnum());
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseBody
+    public GraceJSONResult returnMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        return GraceJSONResult.errorCustom(ResponseStatusEnum.FILE_MAX_SIZE_ERROR);
+    }
+}
