b77c4dec4cdd11f7d22daaa4e75b211f36c9d6d0
SoSo <LuckilyChensy@163.com>
2021-01-09 21:02:01 +0800

    imooc-news-dev 验证码controller 优化

--- imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/PassportControllerApi.java
+++ imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/PassportControllerApi.java
@@ -5,6 +5,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @Api(value = "用户注册登录", tags = {"用户注册登录的controller"})
 public interface PassportControllerApi {
@@ -14,6 +16,6 @@ public interface PassportControllerApi {
      */
     @ApiOperation(value = "获取短信验证码方法的接口", notes = "获取短信验证码方法的接口", httpMethod = "GET")
     @GetMapping("/getSMSCode")
-    public GraceJSONResult getSMSCode();
+    public GraceJSONResult getSMSCode(String mobile, HttpServletRequest request);
 
 }
