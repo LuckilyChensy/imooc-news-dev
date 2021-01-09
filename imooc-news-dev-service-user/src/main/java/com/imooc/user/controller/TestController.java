247147902e767228c5e73c57928ba10e65c34017
SoSo <LuckilyChensy@163.com>
2021-01-09 15:14:50 +0800

    imooc-news-dev 整合redis

--- imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/TestController.java
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/TestController.java
@@ -1,15 +1,22 @@
 package com.imooc.user.controller;
 
 import com.imooc.api.controller.user.TestControllerApi;
+import com.imooc.grace.result.GraceJSONResult;
 import com.imooc.grace.result.IMOOCJSONResult;
+import com.imooc.utils.RedisOperator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController implements TestControllerApi {
 
     final static Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @Autowired
+    private RedisOperator redis;
 
     @Override
     public Object test(){
@@ -23,6 +30,12 @@ public class TestController implements TestControllerApi {
 //        return IMOOCJSONResult.ok();
           return IMOOCJSONResult.ok("hello");
 
+    }
+
+    @GetMapping("/redis")
+    public Object redis() {
+        redis.set("age", "18");
+        return GraceJSONResult.ok(redis.get("age"));
     }
 
 }
