294a2293b174634e1172c26a7423f1fae9da90e4
SoSo <LuckilyChensy@163.com>
2021-01-09 21:01:54 +0800

    imooc-news-dev 验证码controller 优化

--- imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/PassportController.java
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/PassportController.java
@@ -1,17 +1,24 @@
 package com.imooc.user.controller;
 
+import com.imooc.api.controller.BaseController;
 import com.imooc.api.controller.user.PassportControllerApi;
 import com.imooc.api.controller.user.TestControllerApi;
 import com.imooc.grace.result.GraceJSONResult;
 import com.imooc.grace.result.IMOOCJSONResult;
+import com.imooc.utils.IPUtil;
+import com.imooc.utils.RedisOperator;
 import com.imooc.utils.SMSUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
-public class PassportController implements PassportControllerApi {
+@RequestMapping("passport")
+public class PassportController extends BaseController implements PassportControllerApi {
 
     final static Logger logger = LoggerFactory.getLogger(PassportController.class);
 
@@ -19,12 +26,23 @@ public class PassportController implements PassportControllerApi {
     private SMSUtils smsUtils;
 
     @Override
-    public GraceJSONResult getSMSCode() {
+    public GraceJSONResult getSMSCode(String mobile, HttpServletRequest request) {
 
-        String random = "1234";
+        // 获得用户ip
+        String userIp = IPUtil.getRequestIp(request);
 
-        smsUtils.sendSMS("",random);
+        // 根据用户的ip进行限制，限制用户在60秒内只能获得一次验证码
+        redis.setnx60s(MOBILE_SMSCODE + ":" + userIp, userIp);
+
+        // 生成随机验证码并且发送短信
+        String random = (int)((Math.random() * 9 + 1) * 100000) + "";
+//        smsUtils.sendSMS(MyInfo.getMobile(), random);
+
+        // 把验证码存入redis，用于后续进行验证
+        redis.set(MOBILE_SMSCODE + ":" + mobile, random, 30 * 60);
 
         return GraceJSONResult.ok();
+
     }
+
 }
