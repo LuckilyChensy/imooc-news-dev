05e7405d7d629ed9620f552a5b21b9fca478e0de
SoSo <LuckilyChensy@163.com>
2021-03-01 22:52:56 +0800

    工程优化

--- imooc-news-dev-common/src/main/java/com/imooc/exception/GraceException.java
+++ imooc-news-dev-common/src/main/java/com/imooc/exception/GraceException.java
@@ -8,6 +8,7 @@ import com.imooc.grace.result.ResponseStatusEnum;
 public class GraceException {
 
     public static void display(ResponseStatusEnum responseStatusEnum) {
+
         throw new MyCustomException(responseStatusEnum);
     }
 
