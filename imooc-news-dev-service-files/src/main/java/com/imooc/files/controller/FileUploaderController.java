66eabfac7fd85e03b08cf788f129cf7c8bc0bdf8
SoSo <LuckilyChensy@163.com>
2021-03-01 22:53:07 +0800

    工程优化

--- imooc-news-dev-service-files/src/main/java/com/imooc/files/controller/FileUploaderController.java
+++ imooc-news-dev-service-files/src/main/java/com/imooc/files/controller/FileUploaderController.java
@@ -5,12 +5,16 @@ import com.imooc.files.resource.FileResource;
 import com.imooc.files.service.UploaderService;
 import com.imooc.grace.result.GraceJSONResult;
 import com.imooc.grace.result.ResponseStatusEnum;
+import com.imooc.pojo.bo.NewAdminBO;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class FileUploaderController implements FileUploaderControllerApi {
@@ -66,6 +70,26 @@ public class FileUploaderController implements FileUploaderControllerApi {
             return GraceJSONResult.errorCustom(ResponseStatusEnum.FILE_UPLOAD_NULL_ERROR);
         }
 
+    }
+
+    @Override
+    public GraceJSONResult uploadSomeFiles(String userId, MultipartFile[] files) throws Exception {
+        return null;
+    }
+
+    @Override
+    public GraceJSONResult uploadToGridFS(NewAdminBO newAdminBO) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void readInGridFS(String faceId, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    }
+
+    @Override
+    public GraceJSONResult readFace64InGridFS(String faceId, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
     }
 
 }
