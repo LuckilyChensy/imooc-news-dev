c2b2b33b75b086726a172ed30ff8c79a6105fec8
SoSo <LuckilyChensy@163.com>
2021-03-01 22:52:49 +0800

    工程优化

--- imooc-news-dev-service-api/src/main/java/com/imooc/api/config/InterceptorConfig.java
+++ imooc-news-dev-service-api/src/main/java/com/imooc/api/config/InterceptorConfig.java
@@ -1,6 +1,8 @@
 package com.imooc.api.config;
 
 import com.imooc.api.interceptors.PassportInterceptor;
+import com.imooc.api.interceptors.UserActiveInterceptor;
+import com.imooc.api.interceptors.UserTokenInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,11 +16,34 @@ public class InterceptorConfig implements WebMvcConfigurer {
         return new PassportInterceptor();
     }
 
+    @Bean
+    public UserTokenInterceptor userTokenInterceptor() {
+        return new UserTokenInterceptor();
+    }
+
+    @Bean
+    public UserActiveInterceptor userActiveInterceptor() {
+        return new UserActiveInterceptor();
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
         registry.addInterceptor(passportInterceptor())
-                .addPathPatterns("/passport/getSMSCode");
+                .addPathPatterns("/passport/getSMSCode");   // 添加拦截的请求路径
+
+        registry.addInterceptor(userTokenInterceptor())
+                .addPathPatterns("/user/getAccountInfo")
+                .addPathPatterns("/user/updateUserInfo")
+                .addPathPatterns("/fs/uploadFace")
+                .addPathPatterns("/fs/uploadSomeFiles")
+                .addPathPatterns("/fans/follow")
+                .addPathPatterns("/fans/unfollow");
+
+        registry.addInterceptor(userActiveInterceptor())
+                .addPathPatterns("/fs/uploadSomeFiles")
+                .addPathPatterns("/fans/follow")
+                .addPathPatterns("/fans/unfollow");
 
     }
 
